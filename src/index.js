602d860a9c3bc5dc7206f64f4d8914b8a868205e
Oleg <karibyon@gmail.com>
2020-10-02 23:03:34 +0300

    feat: complite towel-sort task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix || matrix.length === 0) return [];
+    let result = [];
+    for( let i = 0; i < matrix.length; i++) {
+        if(i % 2 == 1) {
+            for( let j = matrix[i].length - 1; j >= 0; j--) {
+                console.log(j + ' UP');
+                result.push(matrix[i][j]);
+            }
+        } else {
+            for( let j = 0; j < matrix[i].length; j++) {
+                result.push(matrix[i][j]);
+            }
+        }
+    }
+    return result;
 }
